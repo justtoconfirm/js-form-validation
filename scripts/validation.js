efe016f7b68eb34fb263c3cee47fbe5a8fc0694c
Dan Jackson <dan.jackson@crunch.co.uk>
2018-09-12 13:06:01 +0100

    JavaScript updated to create initial library structure

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,30 @@
+// https://ourcodeworld.com/articles/read/37/how-to-create-your-own-javascript-library
+// https://code.tutsplus.com/tutorials/build-your-first-javascript-library--net-26796
+
+(function(window) {
+
+	// Enable strict mode
+	'use strict';
+
+	// This function will contain all our code
+	function formValidInit() {
+
+		// Generate an empty object within the window object
+		var _formValidObject = {};
+
+		// We will add functions to our library here
+
+		return _formValidObject;
+	}
+
+	// Library needs to be globally accessible, save in window object
+	if(typeof(window.formValid) === 'undefined') {
+		window.formValid = formValidInit();
+	}
+
+})(window);
+
+/*
 var emailField = document.getElementById('emailField');
 var submitButton = document.getElementById('submitButton');
 
@@ -14,3 +41,4 @@ emailField.addEventListener('keyup', function(event) {
 submitButton.addEventListener('click', function(event) {
 	alert('Form Submitted');
 });
+*/
